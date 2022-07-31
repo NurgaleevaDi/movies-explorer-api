da9bfd7d98fdd8d25e9b90967b89ddaa9c9391b0
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-31 12:06:42 +0300

    add errorHandler module

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { limiter } = require('./helpers/rateLimiter');
 const routes = require('./routes');
+const errorsHandler = require('./middlewares/errorsHandler');
 
 const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
 
@@ -12,6 +15,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet());
+
+app.use(limiter);
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb');
 
@@ -23,14 +30,7 @@ app.use(errorLogger);
 
 app.use(errors()); // обработчик ошибок celebrate
 
-// централизованный обработчик ошибок
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode)
-    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-  next();
-});
+app.use(errorsHandler); // централизованный обработчик ошибок
 
 app.listen(PORT, () => {
   // console.log(`App listening on port ${PORT}`);
